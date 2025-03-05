9a0300ba4d01261014bf6ca90ae6f57fe932a6ba
Migasstas.info <migasstas@gmail.com>
2025-03-05 05:23:12 +0200

    05 03 25

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout: 6500,
+    autoplaySpeed: 1000,
+    loop: true,
+    nav: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1100:{
+            items:2
+        }
+    }
+})
+
+$( function() {
+    $( "#accordion" ).accordion();
+} );
